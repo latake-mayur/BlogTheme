25a0fa190df2196d9441a55814084ed46fd03d0d
unknown <latakemayur@gmail.com>
2025-03-27 12:34:58 +0530

    blog page ready

--- functions.php
+++ functions.php
@@ -2,6 +2,17 @@
 
   register_nav_menus([
     "Primary_menu" => ("main menu")
-  ])
+  ]);
+
+
+  
  
+
+
+function mytheme_setup() {
+    add_theme_support('post-thumbnails');
+}
+add_action('after_setup_theme', 'mytheme_setup');
+
+
 ?>
